5dd88a71fac9fdc8a76d835b6a2912e4f4571826
idrisajala2 <ajalaidris90@gmail.com>
2022-02-06 18:04:59 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,222 @@
-import logo from './logo.svg';
-import './App.css';
+  import   React  from 'react'
+  import "./App.css"
+  import svg from "./images/tyk.png"
+  import svg1 from "./images/learning.png"
+  import svg3 from "./images/winter.png"
+  import svg4 from "./images/for-parents.png"
+  import svg5 from "./images/for-teachers.png"
+  import svg6 from "./images/graphic-mobile.png"
+  import svg7 from "./images/icon-junior.png"
+  import svg8 from "./images/graphic-bbc.png"
+  import svg9 from "./images/logo-apple.png"
+  import svg10 from "./images/logo-googleedu.png"
+  import svg11 from "./images/pbs.png"
+  import svg12 from "./images/facebook.png"
+  import svg13 from "./images/bbc.png"
+  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+  const App = ()=>{
+ return (
+    <div>
+      <div class ="hero">
+       <div class = "headercont">
+         <div class="im">
+           <img alt="pdf" src={svg}></img>
+         </div>
+         <div class="sp">
+           <span>PLAY</span>
+           <span>PARENT</span>
+           <span>EDUCATOR</span>
+           <span>WHY CODE?</span>
+         </div>
+         <div class="bt1">
+           <button>JOIN FOR FREE</button>
+           <span>LOG IN</span>
+         </div>
+{/* {........................................................} */}
+      </div>
+      <div class="first">
+      <div class="second">
+       <div class="con">
+         <h1>Coding Made Easy</h1>
+         <p>The fun way to learn programming and develop problem-solving & critical thinking skills! Ages 5-18</p>
+         <div class="get">GET STARTED FOR FREE</div>
+         <p>At-home plans from $9 per month</p>
+         </div>
+         <div class="win">
+           <img alt="pdf" src={svg3}></img>
+         </div>
+         </div> 
+         </div>
+      </div>
+      {/* {..............................................................} */}
+      <div class="kids">
+        <div class="kids2">
+        <div>
+          <h1>The #Coding platform for Kids</h1>
+          <p>tyler powers the creativity of over 60million kids and servers thousands of schools an educators worldwide..</p>
+          <p>Our interactive story-based learning allows kids to learn the basics with easy block-based coding challenges before seamlessly transitioning to real-world text-based languages like JavaScript and Python.</p>
+          <p>With 65+ award-winning courses, there's a learning path for every kid, no matter their age or level. We have over 5,000 learning modules, backed by hundreds of built-in tutorials and hands-on projects.</p>
+          <div class="get">START CODING NOW</div>
+        </div>
+        <div>
+          <img alt="svg" src={svg1}></img>
+        </div>
+      </div>
+      </div>
+      {/* {...........................................} */}
+      <div class="for" >
+        <div class="for2">
+        <div>
+          <img alt="pdf" src={svg4}></img>
+        </div>
+        <div>
+          <h1>Parent</h1>
+          <p>Coding is quickly becoming an essential skill, one that will prepare your kids to succeed in an increasingly digital world.</p>
+          <p>Tynker offers a range of options to learn coding: Your children can learn to code with our self-paced, immersive game-like courses, and then advance to learning real-world Python and JavaScript.</p>
+          <p>Select the option that's best for your child!</p>
+          <button>LEARN MORE</button>
+        </div>
+      </div>
+      </div>
+      {/* {.................................} */}
+      <div class="for" >
+        <div class="pro">
+        <div>
+          <img alt="pdf" src={svg5}></img>
+        </div>
+        <div>
+          <h1>teacher</h1>
+          <p>Coding is quickly becoming an essential skill, one that will prepare your kids to succeed in an increasingly digital world.</p>
+          <p>Tynker offers a range of options to learn coding: Your children can learn to code with our self-paced, immersive game-like courses, and then advance to learning real-world Python and JavaScript.</p>
+          <p>Select the option that's best for your child!</p>
+          <button>LEARN MORE</button>
+        </div>
+      </div>
+      </div>
+      {/* {..........................................................} */}
+      <div class="mbox">
+        <div class="inbox">
+        <h1>3 Highly-rated mobile App for Learning Code</h1>
+        <p>download tynker apps and learn to code on the go</p>
+        <button>LEARN MORE</button>
+        </div>
+  </div>
+      <div class="bigdiv">
+        <div class="smldiv">
+          <div>
+   <img alt="pf" src={svg6}></img>
+          </div>
+          <div class="ty">
+            <div class="tyk">
+            <img alt="pdf" src={svg7}></img>
+            <h2>Tynker Junior</h2>
+            </div>
+            <div class="pa">
+              <p>tap-tap pictures coding with voice-overs pre-readers solve story-based puzzles and build thier first programs in coding sand boxees. Ages 5-7</p>
+            </div>
+            {/* {..................................} */}
+            <div class="tyk">
+            <img alt="pdf" src={svg7}></img>
+            <h2>Tynker </h2>
+            </div>
+            <div class="pa">
+              <p>tap-tap pictures coding with voice-overs pre-readers solve story-based puzzles and build thier first programs in coding sand boxees. Ages 5-7</p>
+            </div>
+            {/* {.......................} */}
+            <div class="tyk">
+            <img alt="pdf" src={svg7}></img>
+            <h2>mod creator</h2>
+            </div>
+            <div class="pa">
+              <p>tap-tap pictures coding with voice-overs pre-readers solve story-based puzzles and build thier first programs in coding sand boxees. Ages 5-7</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* {..................................} */}
+      <div class="min">
+        <h1>a Mini-computer for the Iinternet of Things!</h1>
+        </div>
+        <div class="te">
+        <div class="th">
+          <div>
+          <p>tyler powers the creativity of over 60million kids and servers thousands of schools an educators worldwide..</p>
+          <p>Our interactive story-based learning allows kids to learn the basics with easy block-based coding challenges before seamlessly transitioning to real-world text-based languages like JavaScript and Python.</p>
+          <p>With 65+ award-winning courses, there's a learning path for every kid, no matter their age or level. We have over 5,000 learning modules, backed by hundreds of built-in tutorials and hands-on projects.</p>
+          </div>
+          <div>
+            <img alt="pdf" src={svg8}></img>
+          </div>
+          </div>
+        </div>
+{/* {..........................................} */}
+<div class="why">
+  <div class="why2">
+  <h1>Why should Learn to code with tynker</h1>
+  <p>With 65+ award-winning courses, there's a learning path for every kid, no matter their age or level. We have over 5,000 learning modules, backed by hundreds of built-in tutorials and hands-on projects.Our interactive story-based learning allows kids to learn the basics with easy block-based coding challenges before seamlessly transitioning to real-world text-based languages like JavaScript and Python </p>
+  <p>With 65+ award-winning courses, there's a learning path for every kid, no matter their age or level. We have over 5,000 learning modules, backed by hundreds of built-in tutorials and hands-on projects.Our interactive story-based learning allows kids to learn the basics with easy block-based coding challenges before seamlessly transitioning to real-world text-based languages like JavaScript and Python </p>
+
+  </div>
+</div>
+<div class="hb">
+  <div class="hb2">
+    <div class="cb">
+      <h3>coding Benefit</h3>
+      <div>top 10 reasons to code</div>
+      <div>kids coding Basics</div>
+      <div>Desktop 21th century</div>
+      <div>top 10 reasons to code</div>
     </div>
-  );
-}
+    <div class="cb">
+    <h3>coding guider</h3>
+      <div>top 10 reasons to code</div>
+      <div>kids coding Basics</div>
+      <div>Desktop 21th century</div>
+      <div>top 10 reasons to code</div>
+    </div>
+    <div class="cb">
+    <h3>tynker highlight</h3>
+      <div>top 10 reasons to code</div>
+      <div>kids coding Basics</div>
+      <div>Desktop 21th century</div>
+      <div>top 10 reasons to code</div>
+    </div>
+    <div class="cb">
+    <h3>coding in school</h3>
+      <div>top 10 reasons to code</div>
+      <div>kids coding Basics</div>
+      <div>Desktop 21th century</div>
+      <div>top 10 reasons to code</div>
+    </div>
+  </div>
+</div>
+{/* {...............................................} */}
+ <div class="pfd">
+   <div class="pfd2">
+    <h1>Trusted by Leading Brands</h1>
+    <p>With 65+ award-winning courses, there's a learning path for every kid, no matter their age or level. We have over 5,000 learning modules, backed by hundreds of built-in tutorials and hands-on projects.Our interactive story-based learning allows kids to learn the basics with easy block-based coding challenges before seamlessly transitioning to real-world text-based languages like JavaScript and Python </p>
+    </div>
+ </div>
+ <div class="lags">
+   <div class="lags1"> 
+     <img alt="pdf" src={svg9}></img>
+     <img alt="pdf" src={svg10}></img>
+     <img alt="pdf" src={svg11}></img>
+     <img alt="pdf" src={svg12}></img>
+      <img alt="pdf" src={svg13}></img>
+     
 
+     
+     
+   </div>
+ </div>
+    </div>
+  )
+
+
+  
+}
 export default App;
